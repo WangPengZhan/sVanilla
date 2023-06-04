d100785dd8a1a6892305d6c72f0076b95da1d1c7
ives <1660583890@qq.com>
2023-06-04 20:56:20 +0800

    finished dumpcrash

--- sVanilla/src/main.cpp
+++ sVanilla/src/main.cpp
@@ -1,37 +1,11 @@
 #include <QApplication>
 #include <QMainWindow>
-#include <QFile>
 
-#include <client/windows/handler/exception_handler.h>
-
-bool callback(const wchar_t *dump_path, const wchar_t *id,
-	void *context, EXCEPTION_POINTERS *exinfo,
-	MDRawAssertionInfo *assertion,
-	bool succeeded)
-{
-	QFile file("test.txt");
-	file.open(QIODevice::WriteOnly);
-	std::wstring wstr = dump_path;
-	wstr += L":";
-	wstr += L":";
-	wstr += id;
-	file.write(QString::fromStdWString(wstr).toLocal8Bit());
-	file.close();
-	return succeeded;
-}
-
-static void crashHare()
-{
-    int *a = (int *)(NULL);
-    *a = 1; // 放心的奔溃吧
-}
+#include "Logger/dump.h"
 
 int main(int argc, char *argv[])
 {
-    google_breakpad::ExceptionHandler eh(
-    L"./dump", NULL, callback, NULL,
-    google_breakpad::ExceptionHandler::HANDLER_ALL);
-    crashHare();
+	DumpColletor::registerDumpHandle();
 
     QApplication a(argc, argv);
 
