c21dfb9a222bd49d747d639ef94d8bb6405e891d
iveswang <46638131+WangPengZhan@users.noreply.github.com>
2024-04-08 15:02:06 +0800

    fix plugin error (#34)
    fix plugin error

--- sVanilla/src/Plugin/PluginManager.cpp
+++ sVanilla/src/Plugin/PluginManager.cpp
@@ -1,4 +1,5 @@
 #include <fstream>
+#include <iostream>
 
 #include <nlohmann/json.hpp>
 
@@ -7,12 +8,21 @@
 namespace Plugin
 {
 
-std::string PluginManager::m_pluginDir = "plugin/";
-std::string PluginManager::m_configPath = "config/plugin_config.json";
+std::string const PluginManager::m_pluginDir = "plugin/";
+std::string const PluginManager::m_configPath = "config/plugin_config.json";
+
+#ifdef _WIN32
+std::string const PluginManager::m_dynamicExtension = ".dll";
+#elif __linux__
+std::string const PluginManager::m_dynamicExtension = ".so";
+#elif __APPLE__
+std::string const PluginManager::m_dynamicExtension = ".dylib";
+#endif
 
 PluginManager::PluginManager()
 {
     loadConfig();
+    initPluginPaths();
 }
 
 PluginManager::~PluginManager()
@@ -20,22 +30,53 @@ PluginManager::~PluginManager()
     saveConfig();
 }
 
+std::string PluginManager::pluginDir()
+{
+    return m_pluginDir;
+}
+
 void PluginManager::loadPlugins()
 {
-    std::filesystem::path plugPath(m_pluginDir);
-    std::string allPath = std::filesystem::absolute(plugPath).string();
-    auto pLoader = std::make_shared<DynamicLibLoader>(allPath + "TemplatePlugin.dll");
-    pLoader->loadLibrary();
-    auto plugin = pLoader->loadPluginSymbol();
-    if (plugin)
+    for (const auto& pluginPath : m_pluginsPaths)
     {
-        m_libHandles.insert({"TemplatePlugin", pLoader});
-        m_plugins.insert({"TemplatePlugin", plugin});
+        addPlugin(pluginPath);
     }
 }
 
-void PluginManager::addPlugin(const std::string& pluginName)
+void PluginManager::addPlugin(const std::string& pluginPath)
 {
+    auto pLoader = std::make_shared<DynamicLibLoader>(pluginPath);
+    pLoader->loadLibrary();
+    auto plugin = pLoader->loadPluginSymbol();
+    if (!plugin)
+    {
+        return;
+    }
+
+    PluginConfig pluginConfig;
+    for (const auto& config : m_pluginConfig)
+    {
+        if (config.pluginName == plugin->pluginName())
+        {
+            pluginConfig = config;
+            break;
+        }
+    }
+
+    if (pluginConfig.pluginName.empty())
+    {
+        pluginConfig.enabled = true;
+        pluginConfig.pluginName = plugin->pluginName();
+        pluginConfig.libName = std::filesystem::path(pluginPath).stem().string();
+        pluginConfig.version = plugin->pluginVersion();
+        m_pluginConfig.emplace_back(pluginConfig);
+    }
+    
+    if (pluginConfig.enabled)
+    {
+        m_libHandles.insert({plugin->pluginName(), pLoader});
+        m_plugins.insert({plugin->pluginName(), plugin});
+    }
 }
 
 std::shared_ptr<IPlugin> PluginManager::getPlugin(const std::string& pluginName)
@@ -81,6 +122,19 @@ void PluginManager::saveConfig() const
     nlohmann::json json = m_pluginConfig;
     o << json.dump(4);
     m_configChanged = false;
+}
+
+void PluginManager::initPluginPaths()
+{
+    std::filesystem::path plugPath(m_pluginDir);
+
+    for (const auto& entry : std::filesystem::directory_iterator(plugPath)) 
+    {
+        if (entry.path().extension() == m_dynamicExtension) 
+        {
+            m_pluginsPaths.insert(std::filesystem::absolute(entry.path()).string());
+        }
+    }
 }
 
 }  // namespace  Plugin
