0e8ee30d734889976d8129c269401b998f437d3a
iveswang <1660583890@qq.com>
2024-05-14 00:49:12 +0800

    adjust ffmpeg

--- cmake/qtdeploy.cmake
+++ cmake/qtdeploy.cmake
@@ -1,7 +1,6 @@
-
 # 项目使用
 function(deployqt arg_target)
-    find_program(qt_deployqt_executable NAMES windeployqt macdeployqt HINTS "${qt_bin_directory}")
+    find_program(qt_deployqt_executable NAMES windeployqt macdeployqt linuxdeployqt HINTS "${qt_bin_directory}")
 
     if (NOT EXISTS "${qt_deployqt_executable}")
         message("can not find the deployqt tools")
@@ -37,5 +36,33 @@ function(deployqt arg_target)
                         "$<TARGET_FILE_DIR:${arg_target}>"
             )
         endforeach()
-    endif ()
+    elseif(APPLE)
+        add_custom_command(TARGET ${arg_target} 
+            POST_BUILD 
+                COMMAND
+                    "${qt_deployqt_executable}"
+                    "$<TARGET_FILE:${arg_target}>"
+                    -always-overwrite
+        )
+    elseif(UNIX)
+        add_custom_command(TARGET ${arg_target} 
+            POST_BUILD 
+                COMMAND
+                    "${qt_deployqt_executable}"
+                    "$<TARGET_FILE:${arg_target}>"
+                    -always-overwrite
+                    -unsupported-allow-new-glibc
+        )
+    endif()
+    
+    # 根据find_package后面的component拷贝需要的动态库(防止遗漏)
+    foreach (component IN LISTS qt_components)
+        get_qt_library_target(${component} qt_component_target)
+        add_custom_command(TARGET ${arg_target} 
+            POST_BUILD 
+                COMMAND ${CMAKE_COMMAND} -E copy_if_different
+                    "$<TARGET_FILE:${qt_component_target}>"
+                    "$<TARGET_FILE_DIR:${arg_target}>"
+        )
+    endforeach()
 endfunction()
