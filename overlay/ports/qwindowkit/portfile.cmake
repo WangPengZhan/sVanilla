fd57bd9658f0819bf26f06a843437479c404016d
AlanusMeminius <nononevent@outlook.com>
2024-05-19 11:30:13 +0800

    feature(vcpkg): change submodule to vcpkg

---
+++ overlay/ports/qwindowkit/portfile.cmake
@@ -0,0 +1,46 @@
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO stdware/qwindowkit
+    REF 00b91a374872280a4a5addc22f08c7954a6637aa
+    SHA512 d9fbbe03be7d8b20d09d2ff984330f5c1813827412a17c606eb03203776aaacdd9b4fdb16eade6391a5c6aa5b2bf373ec4a2421774fbb1eb2f3f32f02462f2ab
+)
+
+if (WIN32)
+    if (DEFINED ENV{QT_ROOT_DIR})
+        set(QT_PATH "$ENV{Qt_DIR}" ${CMAKE_PREFIX_PATH})
+    elseif (DEFINED ENV{Qt_DIR})
+        set(QT_PATH "$ENV{Qt_DIR}" ${CMAKE_PREFIX_PATH})
+    elseif (DEFINED ENV{Qt6_DIR})
+        set(QT_PATH "$ENV{Qt6_DIR}" ${CMAKE_PREFIX_PATH})
+    elseif (DEFINED ENV{Qt5_DIR})
+        set(QT_PATH "$ENV{Qt5_DIR}" ${CMAKE_PREFIX_PATH})
+    elseif (DEFINED ENV{PATH})
+        foreach (PATH_ITEM $ENV{PATH})
+            string(TOLOWER "${PATH_ITEM}" PATH_ITEM_LOWER)
+            if (PATH_ITEM_LOWER MATCHES "qt")
+                set(CMAKE_PREFIX_PATH "${PATH_ITEM}" ${CMAKE_PREFIX_PATH})
+                break()
+            endif ()
+        endforeach ()
+    endif ()
+endif ()
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+    -DQWINDOWKIT_BUILD_STATIC=OFF
+    -DQWINDOWKIT_BUILD_WIDGETS=ON
+    -DQWINDOWKIT_BUILD_QUICK=OFF
+    -DCMAKE_PREFIX_PATH=${QT_PATH}
+)
+
+vcpkg_cmake_install()
+vcpkg_cmake_config_fixup(
+    PACKAGE_NAME ${PORT}
+    CONFIG_PATH lib/cmake/QWindowKit
+)
+vcpkg_copy_pdbs()
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}/" RENAME copyright)
---
+++ overlay/ports/vanillastyle/portfile.cmake
@@ -0,0 +1,63 @@
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO AlanusMeminius/VanillaStyle
+    REF 24bec156550abe7ec0ef3788e8ad0719bde44d1b
+    SHA512 b2ecfb9cbdf58d2a59a1d2d5d93cfd6a4512f9382a28ebfc2f2e2364b1a7b8c1bc8e0578401a51f8f68b87aa04029cfa166ab880f745b580749ae72121ba0e99
+)
+file(REMOVE
+    "${SOURCE_PATH}/${PORT}/CMakeLists.txt"
+)
+
+file(COPY
+    "${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt"
+    DESTINATION "${SOURCE_PATH}/${PORT}"
+)
+
+vcpkg_download_distfile(
+    HEADER_FILE
+    URLS "https://raw.githubusercontent.com/bfraboni/FastGaussianBlur/main/fast_gaussian_blur_template.h"
+    FILENAME "fast_gaussian_blur_template.h"
+    SHA512 6c95bfdecdc5c55c3e4c467ea97c90c4a0a8c903257e8e5c76853195282c74b3f4029745e3631d319ac6fe314d9f2a471b01b6f640f634799ba98f927b4ab1f0
+)
+file(COPY
+    ${HEADER_FILE}
+    DESTINATION "${SOURCE_PATH}/${PORT}/include"
+)
+
+if (WIN32)
+    if (DEFINED ENV{QT_ROOT_DIR})
+        set(QT_PATH "$ENV{Qt_DIR}" ${CMAKE_PREFIX_PATH})
+    elseif (DEFINED ENV{Qt_DIR})
+        set(QT_PATH "$ENV{Qt_DIR}" ${CMAKE_PREFIX_PATH})
+    elseif (DEFINED ENV{Qt6_DIR})
+        set(QT_PATH "$ENV{Qt6_DIR}" ${CMAKE_PREFIX_PATH})
+    elseif (DEFINED ENV{Qt5_DIR})
+        set(QT_PATH "$ENV{Qt5_DIR}" ${CMAKE_PREFIX_PATH})
+    elseif (DEFINED ENV{PATH})
+        foreach (PATH_ITEM $ENV{PATH})
+            string(TOLOWER "${PATH_ITEM}" PATH_ITEM_LOWER)
+            if (PATH_ITEM_LOWER MATCHES "qt")
+                set(CMAKE_PREFIX_PATH "${PATH_ITEM}" ${CMAKE_PREFIX_PATH})
+                break()
+            endif ()
+        endforeach ()
+    endif ()
+endif ()
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+    -DCMAKE_PREFIX_PATH=${QT_PATH}
+)
+
+vcpkg_cmake_install()
+
+vcpkg_cmake_config_fixup(
+    PACKAGE_NAME ${PORT}
+    CONFIG_PATH "lib/cmake/VanillaStyle"
+)
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
+
+file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+#configure_file("${CMAKE_CURRENT_LIST_DIR}/usage" "${CURRENT_PACKAGES_DIR}/share/${PORT}/usage" COPYONLY)
