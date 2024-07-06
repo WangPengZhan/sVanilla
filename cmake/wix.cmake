e9fe0b440800aea5b03050c5188be44699973a55
iveswang <1660583890@qq.com>
2024-07-06 11:33:07 +0800

    chore: add parallel compile and adjust license time

--- cmake/wix.cmake
+++ cmake/wix.cmake
@@ -1,8 +1,8 @@
 function(generator_wix)
     if (WIN32)
         configure_file(
-            "${CMAKE_SOURCE_DIR}/cmake/sVanillaInfo.wsi.in"
-            "${CMAKE_BINARY_DIR}/sVanillaInfo.wsi"
+            "${CMAKE_SOURCE_DIR}/cmake/${PROJECT_NAME}Info.wsi.in"
+            "${CMAKE_BINARY_DIR}/${PROJECT_NAME}Info.wsi"
             @ONLY
         )
     endif()
