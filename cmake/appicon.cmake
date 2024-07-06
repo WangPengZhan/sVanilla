f8894a59e8ebca3a5e8d7c4c111af583c8953339
iveswang <46638131+WangPengZhan@users.noreply.github.com>
2024-07-06 12:08:53 +0800

    add parallel compile and adjust license time (#79)
    * chore: add parallel compile and adjust license time

--- cmake/appicon.cmake
+++ cmake/appicon.cmake
@@ -1,11 +1,11 @@
 function(appicon TARGET_NAME RESOURCE_FILES)
     if(WIN32)
-        set(ICON_RESOURCE sVanilla/resource/appIcon/sVanilla.ico)
+        set(ICON_RESOURCE sVanilla/resource/appIcon/${TARGET_NAME}.ico)
         set(RC_FILE ${CMAKE_CURRENT_BINARY_DIR}/${TARGET_NAME}.rc)
-        configure_file(${CMAKE_SOURCE_DIR}/cmake/sVanilla.rc.in ${CMAKE_CURRENT_BINARY_DIR}/${TARGET_NAME}.rc)
+        configure_file(${CMAKE_SOURCE_DIR}/cmake/${TARGET_NAME}.rc.in ${CMAKE_CURRENT_BINARY_DIR}/${TARGET_NAME}.rc)
         set(RESOURCE_FILES ${ICON_RESOURCE} ${TARGET_NAME}.rc PARENT_SCOPE)
     elseif(APPLE)
-        set(ICON_FILE sVanilla/resource/appIcon/sVanilla.icns)
+        set(ICON_FILE sVanilla/resource/appIcon/${TARGET_NAME}.icns)
         get_filename_component(ICON_FILE_NAME ${ICON_FILE} NAME)
         set_target_properties(${PROJECT_NAME} PROPERTIES
             MACOSX_BUNDLE_BUNDLE_NAME ${PROJECT_NAME}
@@ -24,13 +24,13 @@ function(appicon TARGET_NAME RESOURCE_FILES)
         set(ICON_ROOT ${CMAKE_CURRENT_BINARY_DIR}/share)
         set(DESKTOP_FILE ${ICON_ROOT}/applications/${TARGET_NAME}.desktop)
         message("DESKTOP_FILE: ${DESKTOP_FILE}")
-        configure_file(${CMAKE_SOURCE_DIR}/cmake/sVanilla.desktop.in ${DESKTOP_FILE})
+        configure_file(${CMAKE_SOURCE_DIR}/cmake/${TARGET_NAME}.desktop.in ${DESKTOP_FILE})
         file(GENERATE OUTPUT ${DESKTOP_FILE} CONTENT "${DESKTOP_CONTENT}")
         set(ICON_DIR "${ICON_ROOT}/icon/")
         add_custom_command(TARGET ${TARGET_NAME}
             POST_BUILD
                 COMMAND ${CMAKE_COMMAND} -E make_directory ${ICON_DIR}
-                COMMAND ${CMAKE_COMMAND} -E copy_if_different ${CMAKE_SOURCE_DIR}/sVanilla/resource/appIcon/sVanilla.svg ${ICON_DIR}
+                COMMAND ${CMAKE_COMMAND} -E copy_if_different ${CMAKE_SOURCE_DIR}/sVanilla/resource/appIcon/${TARGET_NAME}.svg ${ICON_DIR}
         )
     endif()
 endfunction()
