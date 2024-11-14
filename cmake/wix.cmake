2ce787bea080f1353bc7918bdf3b9b4c660aac70
iveswang <1660583890@qq.com>
2024-11-15 01:04:25 +0800

    feat: add wix

--- cmake/wix.cmake
+++ cmake/wix.cmake
@@ -1,5 +1,8 @@
 function(generator_wix)
     if (WIN32)
+        set(ICON_RESOURCE ${CMAKE_SOURCE_DIR}/sVanilla/resource/appIcon/${TARGET_NAME}.ico)
+        math(EXPR PROJECT_VERSION_NUMBER "${PROJECT_VERSION_MAJOR} * 10000 + ${PROJECT_VERSION_MINOR} * 100 + ${PROJECT_VERSION_PATCH}")
+        message(STATUS "Computed version number: ${PROJECT_VERSION_NUMBER}")
         configure_file(
             "${CMAKE_SOURCE_DIR}/cmake/${PROJECT_NAME}Info.wsi.in"
             "${CMAKE_BINARY_DIR}/${PROJECT_NAME}Info.wsi"
