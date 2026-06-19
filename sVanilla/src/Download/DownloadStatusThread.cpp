31602bc1032a4dc8c09e403b570cee55aeb4cc02
iveswang <1660583890@qq.com>
2026-06-19 20:51:02 +0800

    fix(download): log thread lifecycle and avoid lock hold

--- sVanilla/src/Download/DownloadStatusThread.cpp
+++ sVanilla/src/Download/DownloadStatusThread.cpp
@@ -1,6 +1,8 @@
+#include <utility>
 #include <vector>
 
 #include "DownloadStatusThread.h"
+#include "DownloadLog.h"
 #include <Download/AbstractDownloader.h>
 
 namespace download
@@ -10,6 +12,7 @@ DownloadStatusThread::DownloadStatusThread()
     : m_running(true)
     , m_thread(&DownloadStatusThread::downloadThread, this)
 {
+    DOWNLOAD_LOG_INFO("DownloadStatusThread thread created");
 }
 
 DownloadStatusThread::~DownloadStatusThread()
@@ -43,64 +46,82 @@ void DownloadStatusThread::stop()
 
 void DownloadStatusThread::downloadThread()
 {
+    DOWNLOAD_LOG_INFO("DownloadStatusThread thread start");
     while (m_running.load())
     {
+        std::vector<std::pair<std::string, std::shared_ptr<AbstractDownloader>>> tasks;
         {
             std::unique_lock<std::mutex> lock(m_mutex);
             m_condition.wait(lock, [&] {
                 return !m_downloadTasks.empty() || !m_running.load();
             });
-            std::vector<std::string> removeKeys;
-            for (auto& [key, value] : m_downloadTasks)
-            {
-                switch (value->status())
-                {
-                case AbstractDownloader::Ready:
-                {
-                    value->start();
-                    break;
-                }
-                case AbstractDownloader::Downloading:
-                {
-                    value->downloadStatus();
-                    break;
-                }
-                case AbstractDownloader::Pause:
-                {
-                    value->pause();
-                    break;
-                }
-                case AbstractDownloader::Resumed:
-                {
-                    value->resume();
-                    break;
-                }
-                case AbstractDownloader::Stopped:
-                {
-                    value->stop();
-                    removeKeys.push_back(key);
-                    break;
-                }
-                case AbstractDownloader::Finished:
-                {
-                    value->finish();
-                    removeKeys.push_back(key);
-                    break;
-                }
-                case AbstractDownloader::Error:
-                default:
-                    break;
-                }
-            }
 
-            for (auto& key : removeKeys)
+            tasks.reserve(m_downloadTasks.size());
+            for (const auto& task : m_downloadTasks)
             {
-                m_downloadTasks.erase(key);
+                tasks.push_back(task);
+            }
+        }
+
+        std::vector<std::pair<std::string, std::shared_ptr<AbstractDownloader>>> removeTasks;
+        for (const auto& [key, value] : tasks)
+        {
+            switch (value->status())
+            {
+            case AbstractDownloader::Ready:
+            {
+                value->start();
+                break;
+            }
+            case AbstractDownloader::Downloading:
+            {
+                value->downloadStatus();
+                break;
+            }
+            case AbstractDownloader::Pause:
+            {
+                value->pause();
+                break;
+            }
+            case AbstractDownloader::Resumed:
+            {
+                value->resume();
+                break;
+            }
+            case AbstractDownloader::Stopped:
+            {
+                value->stop();
+                removeTasks.emplace_back(key, value);
+                break;
+            }
+            case AbstractDownloader::Finished:
+            {
+                value->finish();
+                removeTasks.emplace_back(key, value);
+                break;
+            }
+            case AbstractDownloader::Error:
+            default:
+                break;
+            }
+        }
+
+        if (!removeTasks.empty())
+        {
+            std::lock_guard<std::mutex> lock(m_mutex);
+            for (const auto& [key, value] : removeTasks)
+            {
+                auto iter = m_downloadTasks.find(key);
+                if (iter != m_downloadTasks.end() && iter->second == value)
+                {
+                    m_downloadTasks.erase(iter);
+                }
             }
         }
 
         std::this_thread::sleep_for(std::chrono::milliseconds(600));
     }
+    DOWNLOAD_LOG_INFO("DownloadStatusThread thread exit");
 }
 
 }  // namespace download
